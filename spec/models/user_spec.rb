2bfe4d9a6a679a740182842af066b9116137a8eb
839 <8398a7@gmail.com>
2015-06-16 16:32:31 +0900

    refs #16 ユーザモデルの管理者権限に対してのテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,43 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :integer          not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  failed_attempts        :integer          default(0), not null
+#  unlock_token           :string
+#  locked_at              :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  name                   :string           default(""), not null
+#  account                :string           default(""), not null
+#  sex                    :integer
+#  birth                  :date
+#  role                   :integer          default(0), not null
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  context '管理者権限を持っている場合' do
+    let(:user) { FactoryGirl.create(:user, role: 100) }
+    it '#admin?' do
+      expect(user.admin?).to eq true
+    end
+  end
+  context '管理者権限を持っていない場合' do
+    let(:user) { FactoryGirl.create(:user) }
+    it '#admin?' do
+      expect(user.admin?).to eq false
+    end
+  end
 end

b23c56bed8260bfa706fbf3e8bfa96b42b18ef4d
Yuji TANIMOTO <y.tanimoto13@gmail.com>
2015-06-16 16:48:16 +0900

    refs #15 コメント追加

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: group_users
+#
+#  id         :integer          not null, primary key
+#  group_id   :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class GroupUser < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
--- spec/models/group_user_spec.rb
+++ spec/models/group_user_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: group_users
+#
+#  id         :integer          not null, primary key
+#  group_id   :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe GroupUser, type: :model do

f6b5f20972dc1e1609d2596319efe44ef7b13cf8
839 <8398a7@gmail.com>
2015-06-16 16:17:36 +0900

    close #16 ロールによって管理者であるかどうかを判別する

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :lockable
+
+  module Role
+    GENERAL = 0
+    ADMIN = 100
+  end
+
+  def admin?
+    Role::ADMIN <= role
+  end
 end

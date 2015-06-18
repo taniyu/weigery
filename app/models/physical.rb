563bd4b099c24c2afc924b48330d5978810637fd
MaxMEllon <mozi_kke@excite.co.jp>
2015-06-18 21:39:07 +0900

    close #29 before_update でも bmi計算

--- app/models/physical.rb
+++ app/models/physical.rb
@@ -1,6 +1,7 @@
 class Physical < ActiveRecord::Base
   belongs_to :user
   before_save :calc_bmi
+  before_update :calc_bmi
 
   def calc_bmi
     self.bmi = self.weight ** 2 / self.height if self.height != 0

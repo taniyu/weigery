b8e5e169e9150cd92225cf3511e6364c378bd45a
839 <8398a7@gmail.com>
2015-06-27 17:47:35 +0900

    root_pathの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  root to: 'dashboards#index'
+
   resources :users, only: [] do
     resources :physicals, only: [:create, :new, :destroy, :edit, :update, :index]
   end

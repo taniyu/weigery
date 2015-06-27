3c213522d0fd0dc51ddf8ec82f4c4862d697c615
MaxMEllon <mozi_kke@excite.co.jp>
2015-06-27 15:17:29 +0900

    close #44 fix ログイン無しで身体情報が操作できる

--- app/controllers/physicals_controller.rb
+++ app/controllers/physicals_controller.rb
@@ -1,6 +1,6 @@
 class PhysicalsController < ApplicationController
 
-  # before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_user, only: [:new, :create, :edit]
   before_action :set_physical, only: [:edit, :update, :destroy]
 

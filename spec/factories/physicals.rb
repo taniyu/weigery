48ea6c29ee6f81380969678b78a0822e23437b24
MaxMEllon <mozi_kke@excite.co.jp>
2015-06-28 02:25:16 +0900

    close #26 refact factories based on rubocop

--- spec/factories/physicals.rb
+++ spec/factories/physicals.rb
@@ -2,9 +2,9 @@ FactoryGirl.define do
   factory :physical do
     height '170.0'
     weight '51.3'
-    sbp    '80'
-    dbp    '50'
-    pulse  '50'
+    sbp '80'
+    dbp '50'
+    pulse '50'
     measurement_date Date.today
   end
 end

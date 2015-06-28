69c67b8b047f8049d58adc85d2f8e40b865c26a7
839 <8398a7@gmail.com>
2015-06-28 23:15:21 +0900

    refs #62 BMIの計算方法が正しくない不具合を修正

--- app/models/physical.rb
+++ app/models/physical.rb
@@ -1,9 +1,26 @@
+# == Schema Information
+#
+# Table name: physicals
+#
+#  id               :integer          not null, primary key
+#  user_id          :integer          default(0), not null
+#  height           :float            default(0.0), not null
+#  weight           :float            default(0.0), not null
+#  sbp              :integer          default(0), not null
+#  dbp              :integer          default(0), not null
+#  pulse            :integer          default(0), not null
+#  bmi              :float            default(0.0), not null
+#  measurement_date :date             not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Physical < ActiveRecord::Base
   belongs_to :user
   before_save :calc_bmi
   before_update :calc_bmi
 
   def calc_bmi
-    self.bmi = weight / height**2 if height != 0
+    self.bmi = weight / (height / 100.0)**2 if height != 0
   end
 end

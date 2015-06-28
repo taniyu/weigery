f292968c5d011df631402c1e68bc441474a86bec
Yuji TANIMOTO <y.tanimoto13@gmail.com>
2015-06-28 22:46:50 +0900

    refs #6 hashの記法を変更

--- app/models/physical.rb
+++ app/models/physical.rb
@@ -10,30 +10,30 @@ class Physical < ActiveRecord::Base
 
   def self.hw_graph(user_id)
     user_physicals = User.find_by(id: user_id).physicals.order(:measurement_date).limit(7)
-    LazyHighCharts::HighChart.new("graph") do |f|
-      f.title(:text => "身長 & 体重")
-      f.xAxis(:categories => user_physicals.pluck(:measurement_date))
-      f.series(:name => "身長(cm)", :yAxis => 0, :data => user_physicals.pluck(:height))
-      f.series(:name => "体重(kg)", :yAxis => 0, :data => user_physicals.pluck(:weight))
+    LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: '身長 & 体重')
+      f.xAxis(categories: user_physicals.pluck(:measurement_date))
+      f.series(name: '身長(cm)', yAxis: 0, data: user_physicals.pluck(:height))
+      f.series(name: '体重(kg)', yAxis: 0, data: user_physicals.pluck(:weight))
       f.yAxis [
-        {:title => {:text => "身長(cm) 体重(kg)"} }
+        { title: { text: '身長(cm) 体重(kg)' } }
       ]
-      f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical')
+      f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
     end
   end
 
   def self.bpp_graph(user_id)
     user_physicals = User.find_by(id: user_id).physicals.order(:measurement_date).limit(7)
-    LazyHighCharts::HighChart.new("graph") do |f|
-      f.title(:text => "血圧 & 脈拍")
-      f.xAxis(:categories => user_physicals.pluck(:measurement_date))
-      f.series(:name => "最高血圧", :yAxis => 0, :data => user_physicals.pluck(:sbp))
-      f.series(:name => "最低血圧", :yAxis => 0, :data => user_physicals.pluck(:dbp))
-      f.series(:name => "脈拍", :yAxis => 0, :data => user_physicals.pluck(:pulse))
+    LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: '血圧 & 脈拍')
+      f.xAxis(categories: user_physicals.pluck(:measurement_date))
+      f.series(name: '最高血圧', yAxis: 0, data: user_physicals.pluck(:sbp))
+      f.series(name: '最低血圧', yAxis: 0, data: user_physicals.pluck(:dbp))
+      f.series(name: '脈拍', yAxis: 0, data: user_physicals.pluck(:pulse))
       f.yAxis [
-        {:title => {:text => "血圧 脈拍"} }
+        { title: { text: '血圧 脈拍' } }
       ]
-      f.legend(:align => 'right', :verticalAlign => 'top', :y => 75, :x => -50, :layout => 'vertical')
+      f.legend(align: 'right', verticalAlign: 'top', y: 75, x: -50, layout: 'vertical')
     end
   end
 end
